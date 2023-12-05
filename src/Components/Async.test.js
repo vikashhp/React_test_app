943d4578c1a0f4821b50b47fadd9259b121f674b
komalkumari <keshrikomal887@gmail.com>
2023-12-05 13:45:34 -0500

    added

--- src/Components/Async.test.js
+++ src/Components/Async.test.js
@@ -4,6 +4,10 @@ import Async from "./Async";
 describe('Async code',()=>{
 
     test('async code test',async ()=>{
+        window.fetch=jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json:async ()=>[{id:'p1',title:'First post'}]
+        })
         render(<Async/>);
 
         const listItemsElement=await screen.findAllByRole('listitem');
