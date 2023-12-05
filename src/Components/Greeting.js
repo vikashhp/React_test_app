c25c40ad3293551f47f83c52f66feb3afa5c6c34
komalkumari <keshrikomal887@gmail.com>
2023-12-04 19:12:27 -0500

    added

--- src/Components/Greeting.js
+++ src/Components/Greeting.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 
+const Greeting = () => {
+  const [changeText, setChangeText] = useState(false);
 
-const Greeting=()=>{
-return <div>
-    <h2>Hello World!</h2>
-    <p>Let's do it Together</p>
-</div>
-}
+  const textChangeHandler = () => {
+    setChangeText(true);
+  };
+
+  return (
+    <div>
+      <h2>Hello World!</h2>
+     <p>Good to see You</p>
+      {changeText && <p>Changed!</p>}
+      <button onClick={textChangeHandler}>Change_Text!</button>
+    </div>
+  );
+};
 
 export default Greeting;
--- src/Components/Greeting.test.js
+++ src/Components/Greeting.test.js
@@ -2,7 +2,9 @@
 // import App from './App';
 
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import Greeting from "./Greeting";
+
 
 describe("Greeting_Components", () => {
   test("testing helloworld", () => {
@@ -10,6 +12,37 @@ describe("Greeting_Components", () => {
     const helloworld = screen.getByText("Hello World", { exact: false });
     expect(helloworld).toBeInTheDocument();
   });
+
+  test('renders "good to see you" if the button was not Clicked',()=>{
+    render(<Greeting />);
+    const outPutElement = screen.getByText("good to see you", { exact: false });
+    expect(outPutElement).toBeInTheDocument();
+  });
+
+  test('renders "Changed!" if the button was clicked',()=>{
+    render(<Greeting />);
+
+    const buttonElement=screen.getByRole('button');
+
+    userEvent.click(buttonElement);
+
+    const outPutElement = screen.getByText("Changed!",{exact:false});
+    expect(outPutElement).toBeInTheDocument();
+
+  })
+
+  test('does not render "good to see you" if the button was clicked',()=>{
+    render(<Greeting />);
+
+    const buttonElement=screen.getByRole('button');
+
+    userEvent.click(buttonElement);
+
+    const outPutElement = screen.queryByText("good to see you",{exact:false});
+    expect(outPutElement).toBeNull();
+  })
+
+ 
 });
 
 // test('renders learn react link', () => {
